1580e5745b0e198d61298fd0c26f1ca9a2a0362a
HolyHit <33451621+HolyHit@users.noreply.github.com>
2017-11-13 16:42:58 +0100

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,5 @@
+# We kinda just learned how Comments work in the first Exercise.
+print "I could have a code like this." #Cool stuff
+
+# Disabling is something I use often to see what works
+print Ye boi.
---
+++ Ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Keanu-Rico Brueggemann'
+my_age = 20
+my_height = 187 #cm
+my_weight = 67 #Kg
+my_eyes = 'Dunno'
+my_teeth = 'White' #duh
+my_hair = 'Pink'
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." %my_height
+print "He's %d pounds heavy." %my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s." %my_teeth
+
+# this line is tricky?
+print "If I add %d, %d, and %d I get %d." % (
+      my_age, my_height, my_weight, my_age + my_height + my_weight)
